2775b7a4415d9170577a549a8c0882150f51c19b
Bohdan <zinyshen24@gmail.com>
2024-12-17 20:42:12 +0200

    Создание задачи на обработку изображения.

--- vmake_api_requests.py
+++ vmake_api_requests.py
@@ -13,3 +13,28 @@ class VmakeAPI:
         self.vmake_api_key = vmake_api_key
         self.base_url = base_url
         self.check_interval = check_interval
+
+
+        def create_task(self, image_url):
+            """
+            Создание задачи на обработку изображения.
+            :param image_url: URL изображения для обработки.
+            :return: ID задачи или None, если произошла ошибка.
+            """
+            data = {"image": image_url}
+            headers = {"X-Api-Key": self.vmake_api_key}
+
+            response = requests.post(self.base_url, json=data, headers=headers)
+            if response.status_code != 200:
+                print("Ошибка при создании задачи:", response.text)
+                return None
+
+            response_data = response.json()
+            print("Ответ от сервера (создание задачи):", response_data)
+
+            task_id = response_data.get("data", {}).get("taskId")
+            if not task_id:
+                print("Ошибка: не удалось получить ID задачи.")
+                return None
+
+            return task_id
